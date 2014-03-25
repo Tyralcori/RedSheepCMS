50e310ce7aba9182ce7c21a779989e42913638a9
Alexander Czichelski <a.czichelski@satzmedia.de>
2014-03-25 09:09:15 +0100

    Gitignore

--- application/logs/2014/03/25.php
+++ application/logs/2014/03/25.php
@@ -6,3 +6,21 @@
 2014-03-25 02:42:43 --- EMERGENCY: ErrorException [ 1 ]: Cannot use object of type Model_Viewport as array ~ MODPATH/redsheepcore/classes/Kohana/Redsheepcore.php [ 124 ] in file:line
 2014-03-25 02:42:43 --- DEBUG: #0 [internal function]: Kohana_Core::shutdown_handler()
 #1 {main} in file:line
+2014-03-25 03:07:30 --- EMERGENCY: Twig_Error_Runtime [ 0 ]: An exception has been thrown during the rendering of a template ("Unable to write in the cache directory (/var/www/ERO/GIT/RedSheepCMS/modules/twig/cache/17/7c).") in "redsheepstudios/frontend/index" at line 3. ~ MODPATH/twig/vendor/twig/lib/Twig/Template.php [ 279 ] in /var/www/ERO/GIT/RedSheepCMS/modules/twig/vendor/twig/lib/Twig/Template.php:238
+2014-03-25 03:07:30 --- DEBUG: #0 /var/www/ERO/GIT/RedSheepCMS/modules/twig/vendor/twig/lib/Twig/Template.php(238): Twig_Template->displayWithErrorHandling(Array, Array)
+#1 /var/www/ERO/GIT/RedSheepCMS/modules/twig/cache/73/bd/e2ae7b90929e297e35c91308e05d.php(26): Twig_Template->display(Array)
+#2 /var/www/ERO/GIT/RedSheepCMS/modules/twig/vendor/twig/lib/Twig/Template.php(264): __TwigTemplate_73bde2ae7b90929e297e35c91308e05d->doDisplay(Array, Array)
+#3 /var/www/ERO/GIT/RedSheepCMS/modules/twig/vendor/twig/lib/Twig/Template.php(238): Twig_Template->displayWithErrorHandling(Array, Array)
+#4 /var/www/ERO/GIT/RedSheepCMS/modules/twig/vendor/twig/lib/Twig/Template.php(249): Twig_Template->display(Array)
+#5 /var/www/ERO/GIT/RedSheepCMS/modules/twig/vendor/twig/lib/Twig/Environment.php(288): Twig_Template->render(Array)
+#6 /var/www/ERO/GIT/RedSheepCMS/modules/twig/classes/Kohana/Twig.php(120): Twig_Environment->render('redsheepstudios...', Array)
+#7 /var/www/ERO/GIT/RedSheepCMS/system/classes/Kohana/View.php(228): Kohana_Twig->render()
+#8 /var/www/ERO/GIT/RedSheepCMS/system/classes/Kohana/Response.php(160): Kohana_View->__toString()
+#9 /var/www/ERO/GIT/RedSheepCMS/application/classes/Controller/Redsheep.php(35): Kohana_Response->body(Object(Twig))
+#10 /var/www/ERO/GIT/RedSheepCMS/system/classes/Kohana/Controller.php(84): Controller_Redsheep->action_index()
+#11 [internal function]: Kohana_Controller->execute()
+#12 /var/www/ERO/GIT/RedSheepCMS/system/classes/Kohana/Request/Client/Internal.php(97): ReflectionMethod->invoke(Object(Controller_Redsheep))
+#13 /var/www/ERO/GIT/RedSheepCMS/system/classes/Kohana/Request/Client.php(114): Kohana_Request_Client_Internal->execute_request(Object(Request), Object(Response))
+#14 /var/www/ERO/GIT/RedSheepCMS/system/classes/Kohana/Request.php(986): Kohana_Request_Client->execute(Object(Request))
+#15 /var/www/ERO/GIT/RedSheepCMS/index.php(119): Kohana_Request->execute()
+#16 {main} in /var/www/ERO/GIT/RedSheepCMS/modules/twig/vendor/twig/lib/Twig/Template.php:238
