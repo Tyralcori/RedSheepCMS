ba96944381c814b3b608f8ae6776d13159280e60
Alexander Czichelski <a.czichelski@satzmedia.de>
2014-04-04 10:03:20 +0200

    Request and Response Object for plugins, Bugtracker

---
+++ application/classes/Model/bugtracker.php
@@ -0,0 +1,14 @@
+<?php
+
+defined('SYSPATH') or die('No direct access allowed.');
+
+/**
+ * Bugtracker model
+ * @author Alexander Czichelski <a.czichelski@elitecoder.eu>
+ * @version 
+ * @todo 
+ * @since 2014/04/04
+ */
+class Model_Bugtracker extends ORM {
+
+}
--- application/classes/Plugins/Community/RedSheepBugTracker/Bootstrap.php
+++ application/classes/Plugins/Community/RedSheepBugTracker/Bootstrap.php
@@ -8,7 +8,7 @@
  * @since 2014/04/04
  */
 class Plugins_Community_RedSheepBugTracker_Bootstrap extends Controller implements Plugins_Interface {
-    
+
     /**
      * Index function
      * @return string
@@ -17,15 +17,16 @@ class Plugins_Community_RedSheepBugTracker_Bootstrap extends Controller implemen
      */
     public static function index() {
         return array(
-            'status' => 'success', 
+            'status' => 'success',
             'message' => array(
                 'view' => self::view(),
                 'style' => self::style(),
                 'script' => self::script(),
+                'tickets' => self::getOverview(),
             )
         );
     }
-    
+
     /**
      * View function
      * @return boolean
@@ -34,10 +35,10 @@ class Plugins_Community_RedSheepBugTracker_Bootstrap extends Controller implemen
      */
     public static function view() {
         $template = file_get_contents(__DIR__ . '/view/frontend/index.html');
-        
+
         return $template;
     }
-    
+
     /**
      * CSS Return function
      * @return boolean
@@ -49,7 +50,7 @@ class Plugins_Community_RedSheepBugTracker_Bootstrap extends Controller implemen
 
         return $cssPath;
     }
-    
+
     /**
      * JS Return function
      * @return boolean
@@ -61,7 +62,7 @@ class Plugins_Community_RedSheepBugTracker_Bootstrap extends Controller implemen
 
         return $jsPath;
     }
-    
+
     /**
      * Return all the meta infos
      * @return boolean
@@ -77,7 +78,7 @@ class Plugins_Community_RedSheepBugTracker_Bootstrap extends Controller implemen
             'publisherHomepage' => 'http://redsheepstudios.com/',
         );
     }
-    
+
     /**
      * Install function
      * @return boolean
@@ -88,12 +89,12 @@ class Plugins_Community_RedSheepBugTracker_Bootstrap extends Controller implemen
         // Staticsite create        
         $staticSite = ORM::factory('staticsite');
         $staticSite->name = 'bugtracker';
-        $staticSite->text = '&nbsp;';
+        $staticSite->text = file_get_contents(__DIR__ . '/install/overview.html');
         $staticSite->save();
-        
+
         // StaticSite Id
         $getLastStaticSiteID = ORM::factory('staticsite')->where('name', '=', 'bugtracker')->find();
-        
+
         // Viewport create
         $viewport = ORM::factory('viewport');
         $viewport->name = 'bugtracker';
@@ -103,11 +104,13 @@ class Plugins_Community_RedSheepBugTracker_Bootstrap extends Controller implemen
         $viewport->isActive = 1;
         $viewport->position = 'bottom';
         $viewport->save();
-        
+
+        // Generate model
+        // ...
         // Import bugtracker table
         // ...
     }
-    
+
     /**
      * UnInstall function
      * @return boolean
@@ -118,12 +121,56 @@ class Plugins_Community_RedSheepBugTracker_Bootstrap extends Controller implemen
         // Destroy staticsite
         $staticSite = ORM::factory('staticsite')->where('name', '=', 'bugtracker')->find();
         $staticSite->delete();
-        
+
         // Destroy viewport
         $viewport = ORM::factory('viewport')->where('name', '=', 'bugtracker')->find();
         $viewport->delete();
-        
+
+        // Unlink model
+        // ...
         // Drop table
         // ...
     }
+
+    /**
+     * Return overview from all/special ticket(s)
+     * @return boolean
+     * @author Alexander Czichelski <a.czichelski@elitecoder.eu>
+     * @since 2014/04/04
+     */
+    public static function getOverview() {
+        // Get current overview
+        $request = Redsheepcore::getRequestObject();
+        $_uri = str_replace('/', '', $request::detect_uri());
+
+        // Set ticketID
+        $ticketID = (int) str_replace('bugtracker', '', $_uri);
+
+        // Check, if uri is bugtracker
+        if ($_uri != 'bugtracker' && empty($ticketID)) {
+            return false;
+        }
+
+        // Declare Container
+        $ticketContainer = array();
+        
+        // If no id, load all, else load one by id
+        if (empty($ticketID)) {
+            // Load all tickets
+            $ticketContainer = ORM::factory('bugtracker')->find_all()->as_array();
+        } else {
+            // Load ticket by id
+            $ticketContainer = ORM::factory('bugtracker')->where('id', '=', $ticketID)->find()->as_array();
+        }
+        
+        // Return tickets
+        Redsheepcore::setTemplate('bugTrackerReturn', array(
+            'ticketCount' => count($ticketContainer),
+            'tickets' => is_array($ticketContainer) ? $ticketContainer : array($ticketContainer),
+        ));
+        
+        // Template
+        return true;
+    }
+
 }
