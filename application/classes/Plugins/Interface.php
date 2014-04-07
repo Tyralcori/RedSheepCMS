e5065e9aff533e9b0fbe79f69faece1600e1923e
Alexander Czichelski <a.czichelski@satzmedia.de>
2014-04-07 11:19:30 +0200

    Plugin Interface comments

--- application/classes/Plugins/Interface.php
+++ application/classes/Plugins/Interface.php
@@ -8,20 +8,28 @@
  * @since 2014/03/20
  */
 interface Plugins_Interface {
+    // This function get called as "init"
     public static function index();
     
+    // Return the HTML View
     public static function view();
     
+    // Return Style
     public static function style();
     
+    // Return Script
     public static function script();
     
+    // Return Meta Data like Name, version, publisher, etc.
     public static function getMeta();
     
+    // Return config settings
     public static function getConfig();
     
+    // Executed on install
     public static function install();
     
+    // Executed on uninstall
     public static function uninstall();
 }
 ?>
